c176a6d1fbbedd002ae5291648074004b5b89182
caseywescott <44890312+caseywescott@users.noreply.github.com>
2022-11-20 15:44:09 -0800

    Implemented Indexed Mapping of Modes

--- mumu_music.ts
+++ mumu_music.ts
@@ -1,100 +1,72 @@
-// Define a 12 note octave base
-// For Microtonal mode definition, change the OCTAVEBASE and represent scales as intervallic ratios summing to OCTAVEBASE
+/*
+ Define a 12 note octave base
+ For Microtonal mode definition, change the OCTAVEBASE and represent scales as intervallic ratios summing to OCTAVEBASE
+*/
 
 export const octavebase:number = 12; 
 
-/*********************************************************************************************
-//  Mode & Key Definitions
-//
-// We define Scales/Modes as an ordered array of ascending interval steps
-//
-// Example 1: [do, re, me, fa, sol, la, ti] in C Major Key -> C,D,E,F,G,A,B -> Modal Steps: [2,2,1,2,2,1]
-//
-// It is from these defined steps that we can compute a 'Key' AKA Pitches of a Mode at a given Note Base
-//
-// For microtonal scales, steps should be defined as ratios of BASEOCTAVE
-// NOTE: SCALE DEGREESS USE ZERO BASED COUNTING (unlike music theory literature)
-/**********************************************************************************************/
+/*
+ MODE & KEY DEFINITIONS
 
-/* 
+ We define Scales/Modes as an ordered array of ascending interval steps
+
+ Example 1: [do, re, me, fa, sol, la, ti] in C Major Key -> C,D,E,F,G,A,B -> Modal Steps: [2,2,1,2,2,1]
+
+ It is from these defined steps that we can compute a 'Key' AKA Pitches of a Mode at a given Note Base
+
+ For microtonal scales, steps should be defined as ratios of BASEOCTAVE
+ NOTE: SCALE DEGREESS USE ZERO BASED COUNTING (unlike music theory literature)
+
  Modes arranged by the cycle of fifths
  It'll be good to use modes with few/no avoid notes to make sure we are always sounding musical: https://en.wikipedia.org/wiki/Avoid_note
  Lydian, Pentatonic and Dorian would be great choices
 */
 
-//const major_steps = [2,2,1,2,2,2,1] as const; //avoid notes scale degree 3
-export var major_steps = [2,2,1,2,2,2,1]; //avoid notes scale degree 3
+export let modes: { [name: string]: number[] } = {};
 
-const mixolydian_steps = [2,2,1,2,2,1,2] as const; //avoid notes scale degree 3
-const dorian_steps = [2,1,2,2,2,1,2] as const; //avoid notes scale degree 5 but modern ears usually like it
-const aeolian_steps = [2,1,2,2,2,2,2] as const; // avoid notes scale degree 5
-const phrygian_steps = [1,2,2,2,1,2,2] as const; // avoid notes scale degree 1 & 5
-const locrian_steps = [1,2,2,1,2,2,2] as const; // avoid notes scale degree 1
-const lydian_steps = [2,2,2,1,2,2,1] as const; // No avoid notes
+  modes.major = [2,2,1,2,2,2,1]; //avoid notes scale degree 3
+  modes.mixolydian = [2,2,1,2,2,1,2]; //avoid notes scale degree 3
+  modes.dorian = [2,1,2,2,2,1,2]; //avoid notes scale degree 5 but modern ears usually like it
+  modes.aeolian = [2,1,2,2,2,2,2]; // avoid notes scale degree 5
+  modes.phrygian = [1,2,2,2,1,2,2]; // avoid notes scale degree 1 & 5
+  modes.locrian = [1,2,2,1,2,2,2]; // avoid notes scale degree 1
+  modes.lydian = [2,2,2,1,2,2,1]; // No avoid notes
 
-// Additional Modes
+  modes.mixolydian_plus_11 = [2,2,2,1,2,1,2]; //no avoid notes
+  modes.melodicminor = [2,1,2,2,2,2,1]; //no avoid notes
+  modes.harmonicminor = [2,1,2,2,1,3,1];
+  modes.naturalminor = [2,1,2,2,2,2,2];
+  modes.chromatic = [1,1,1,1,1,1,1,1,1,1,1]; 
+  modes.pentatonic = [2,2,3,2,3]; // No avoid notes
 
-const melodicminor_steps =  [2,1,2,2,2,2,1] as const; //no avoid notes
-const harmonicminor_steps = [2,1,2,2,1,3,1] as const;
-const naturalminor_steps = [2,1,2,2,2,2,2] as const;
-const chromatic_steps = [1,1,1,1,1,1,1,1,1,1,1] as const; 
-const pentatonic_steps = [2,2,3,2,3] as const; // No avoid notes
+export let cycle_of_fifths_modes: { [name: string]: number[] } = {};
 
-//***************************************************************
-// Indexed map to mode steps
-// Helpful for Mapping to UIs/Clients and Modal Modulation
-//***************************************************************
-
-const cycle_of_fifths_modes = [
-                  major_steps,
-                  mixolydian_steps,
-                  dorian_steps,
-                  aeolian_steps,
-                  phrygian_steps,
-                  locrian_steps,
-                  lydian_steps
-                  ] as const;
-
-const mode_steps = [
-                  major_steps,
-                  mixolydian_steps,
-                  dorian_steps,
-                  aeolian_steps,
-                  phrygian_steps,
-                  locrian_steps,
-                  lydian_steps,
-                  melodicminor_steps,
-                  harmonicminor_steps,
-                  naturalminor_steps,
-                  chromatic_steps,
-                  pentatonic_steps
-                  ] as const;
-
-                  
-
-                  type ModeMap = {[key: string]: number[]};
-
-                  
-
+  cycle_of_fifths_modes.major = modes.major;
+  cycle_of_fifths_modes.mixolydian = modes.mixolydian;
+  cycle_of_fifths_modes.dorian = modes.dorian;
+  cycle_of_fifths_modes.aeolian = modes.aeolian;
+  cycle_of_fifths_modes.phrygian = modes.phrygian;
+  cycle_of_fifths_modes.locrian = modes.locrian;
+  cycle_of_fifths_modes.lydian = modes.lydian;
 
 /*
-Modes sorted by lighter to dark quality:
-https://guitarchitecture.org/2011/10/02/the-guitarchitect%E2%80%99s-guide-to-modes-part-the-circle-of-5ths-modal-interchange-and-making-the-most-of-one-pattern/
+ Modes sorted by lighter to dark quality:
+ https://guitarchitecture.org/2011/10/02/the-guitarchitect%E2%80%99s-guide-to-modes-part-the-circle-of-5ths-modal-interchange-and-making-the-most-of-one-pattern/
 */
 
-const modes_lighter_to_dark = [
-                  //pentatonic_steps,
-                  lydian_steps,
-                  major_steps,
-                  mixolydian_steps,
-                  dorian_steps,
-                  aeolian_steps,
-                  phrygian_steps,
-                  locrian_steps
-                  ] as const;         
+export let lighter_to_dark_modes: { [name: string]: number[] } = {};
+
+  lighter_to_dark_modes.lydian = modes.lydian;
+  lighter_to_dark_modes.major = modes.major;
+  lighter_to_dark_modes.mixolydian = modes.mixolydian;
+  lighter_to_dark_modes.dorian = modes.dorian;
+  lighter_to_dark_modes.aeolian = modes.aeolian;
+  lighter_to_dark_modes.phrygian = modes.phrygian;
+  lighter_to_dark_modes.locrian = modes.locrian;
+      
 
 /*
-Current PitchClass map for the 7X7 Matrix grid emulating Guqin Fretboard. Ultimately these will be recomputed by formulas to reduce transposistions/computations
+ Current PitchClass map for the 7X7 Matrix grid emulating Guqin Fretboard. Ultimately these will be recomputed by formulas to reduce transposistions/computations
 */
                  
 var guqin_grid_notes = [
@@ -109,13 +81,13 @@ var guqin_grid_notes = [
                   ];
 
 /*
-To select chord progressions that sound good, select from any 6 adjacent cells of the chord_map below. To modulate keys safely, rotate 6 adjacent cells along the cells columns. 
-The further the distance between cell sets (max-distance = 6), the more distant the chords harmonically.
-There are also modal chord changes that also sound good (in the audio demo 0, I modulated down modally by 2 steps)
-Applying this to formulas, chord changes could occur when a clover occurs, while possible modulations to different 
-keys could occur when a smash or steam events occur. This follows the sequential nature of the formulas as each provides the harmonic context for the next. 
-Put another way - The ear needs to traverse a harmonic space to build contexts for modulations to other spaces to occur. 		
-more info at: https://ledgernote.com/columns/music-theory/circle-of-fifths-explained/
+ To select chord progressions that sound good, select from any 6 adjacent cells of the chord_map below. To modulate keys safely, rotate 6 adjacent cells along the cells columns. 
+ The further the distance between cell sets (max-distance = 6), the more distant the chords harmonically.
+ There are also modal chord changes that also sound good (in the audio demo 0, I modulated down modally by 2 steps)
+ Applying this to formulas, chord changes could occur when a clover occurs, while possible modulations to different 
+ keys could occur when a smash or steam events occur. This follows the sequential nature of the formulas as each provides the harmonic context for the next. 
+ Put another way - The ear needs to traverse a harmonic space to build contexts for modulations to other spaces to occur. 		
+ more info at: https://ledgernote.com/columns/music-theory/circle-of-fifths-explained/
 */
 
 const chord_map = [
@@ -141,18 +113,19 @@ export function get_chords_at_idx(idx: number) {
   return chords; 
   } 
 
-//*****************************************************************************************************************
-// PitchClass and Note Utils 
-//
-// Defintions:
-// Note - Integer representation of pitches % OCTAVEBASE. Example E Major -> [1,3,4,6,8,9,11]  (C#,D#,E,F#,G#,A,B)
-// Keynum - Integer representing MIDI note. Keynum = Note * (OCTAVEBASE * OctaveOfNote)
-// Mode - Distances between adjacent notes within an OCTAVEBASE. Example: Major Key -> [2,2,1,2,2,2,1]
-// Key  - A Mode transposed at a given pitch base
-// Tonic - A Note transposing a Mode
-// Modal Transposition - Moving up or down in pitch by a constant interval within a given mode
-// Scale Degree - The position of a particular note on a scale relative to the tonic
-//*****************************************************************************************************************
+/*
+ PITCHCLASS & NOTE UTILS
+
+ Defintions:
+
+ Note - Integer representation of pitches % OCTAVEBASE. Example E Major -> [1,3,4,6,8,9,11]  (C#,D#,E,F#,G#,A,B)
+ Keynum - Integer representing MIDI note. Keynum = Note * (OCTAVEBASE * OctaveOfNote)
+ Mode - Distances between adjacent notes within an OCTAVEBASE. Example: Major Key -> [2,2,1,2,2,2,1]
+ Key  - A Mode transposed at a given pitch base
+ Tonic - A Note transposing a Mode
+ Modal Transposition - Moving up or down in pitch by a constant interval within a given mode
+ Scale Degree - The position of a particular note on a scale relative to the tonic
+*/
 
 export class PitchClass {
   note: number;
@@ -197,10 +170,10 @@ class PitchCollection {
   }
 }
 
-//*************************************************************************************
-// MusicGridState - Manages musical attributes modulated by formulas. 
-// Useful for identifying modulation distance from previous mode/state
-//*************************************************************************************
+/*
+ MusicGridState - Manages musical attributes modulated by formulas. 
+ Useful for identifying modulation distance from previous mode/state
+*/
 
 class MusicGridState {
   chordmap_idx: number;
@@ -226,15 +199,15 @@ class MusicGridState {
 
 }
 
-/************************************************************************************************************************
-// Modal Transpostion 
-//
-// In order to compute modal transposition of a note given a mode and stepnum, 
-// one must calculate the distance of the transposition in steps from a given scale degree
-//
-// Example: transposing the note D in the key of C major by 2 steps is F. The distance of that transposition == 4 steps
-// get back here the thing to test is collapse both transposition functions in to one
-//************************************************************************************************************************/
+/*
+ MODAL TRANSPOSITION
+
+ In order to compute modal transposition of a note given a mode and stepnum, 
+ one must calculate the distance of the transposition in steps from a given scale degree
+
+ Example: transposing the note D in the key of C major by 2 steps is F. The distance of that transposition == 4 steps
+ get back here the thing to test is collapse both transposition functions in to one
+*/
 
 //Compute and Return notes of mode at note base - note base is omitted
 
@@ -263,8 +236,10 @@ export function get_notes_of_key(pc: PitchClass, mode: Array<number>) {
    return notes; 
  }
 
-// Returns the scale degree of a note, given a tonic and mode (-1 if not found)
-// In this implementation, Scale degrees use zero-based counting, unlike in prevalent music theory literature          
+/* 
+ Returns the scale degree of a note, given a tonic and mode (-1 if not found)
+ In this implementation, Scale degrees use zero-based counting, unlike in prevalent music theory literature 
+*/         
 
 export function get_scale_degree(pc: PitchClass, tonic: PitchClass, mode: Array<number>) { 
   var summ:number = 0; 
@@ -375,11 +350,11 @@ export function num_steps_from_scale_degree_down2(scale_degree: number, num_step
    return keynum + num_steps; 
  }
 
-/********************************************************************************************
-// Bi-Modal Transpostion 
-// Functions to calculate transposition of a PitchClass from (tonic+mode) to a (tonic+mode) 
-// Useful for changing keys when certain alchemy events occur
-/********************************************************************************************/
+/*
+ Bi-Modal Transpostion 
+ Functions to calculate transposition of a PitchClass from (tonic+mode) to a (tonic+mode) 
+ Useful for changing keys when certain alchemy events occur
+*/
 
 export function bi_modaltransposition(keynum: number, num_steps: number, tonic: PitchClass, mode: Array<number>) { 
   
@@ -396,24 +371,3 @@ export function bi_modaltransposition(keynum: number, num_steps: number, tonic: 
 
    return keynum + num_steps; 
  }
-
-
-//**********************************************************************************************************
-    Quick Testing
-
-    let pc = new PitchClass(9,4);
-    let tonic = new PitchClass(0,4);
-   
-    console.log('mode_notes_above_note_base: ' + (mode_notes_above_note_base(69, major_steps ))); 
-    console.log('pc_to_keynum: ' + pc.pcToKeynum()); 
-    console.log('keynumToNote: ' + pc.keynumToNote(69)); 
-    console.log('keynumToPitchClass -> note: ' + keynumToPitchClass(69).note); 
-    console.log('get_notes_of_key: ' + get_notes_of_key(pc, major_steps)); 
-    console.log('get_scale_degree: ' + get_scale_degree(pc, tonic, major_steps)); 
-    console.log('keynum_to_scale_degree: ' + keynum_to_scale_degree(69, tonic, major_steps)); 
-    console.log('num_steps_from_scale_degree: ' + num_steps_from_scale_degree(0, 4, tonic, major_steps)); 
-    console.log('modaltransposition: ' + modaltransposition(69, 4, tonic, major_steps)); 
-//***********************************************************************************************************/
-
-
-
