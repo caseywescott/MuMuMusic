b4cbbb97b71909b5e365054d7797a69bd10466e7
caseywescott <44890312+caseywescott@users.noreply.github.com>
2022-11-26 22:23:25 -0800

    Updated FretBoard to allow N-Frets, added comments

--- FretBoard.ts
+++ FretBoard.ts
@@ -3,13 +3,14 @@ import { keynumToPitchClass, num_steps_from_scale_degree, PitchClass } from "./P
 /* 
 This function calculates the notes of any zither/guitar FretBoard for N Strings in any Tuning:
 
-We can use this to calculate a PitchClass Matrix (AKA FretBoard) where only notes contained in a specified mode can be played. 
-Thid enables Mech's traversing the grid to map to musicals notes.
+We can use this to calculate Calculate a PitchClass Matrix (AKA FretBoard) where only notes contained in a specified mode can be played. 
+When Mech's traverse the grid, musicals notes are selected.
 
 Arguments: 
 
     Name 
     String Steps - Specify Number of modal steps between each open string note for N zither strings (Violins/Guitars are tuned to fourths)
+    Num_Frets - Number of Frets to generate. For MuMu, Num_Frets == 7
     Scaledegree - The scaledegree of a given tonic/mode to build the fretboard
     Tonic - Pitch base of the tuning
     Mode - Mode used to tune Frets and steps to increment fret values
@@ -20,7 +21,7 @@ EG: Standard Guitar Tuning:
 
 EG: Standard Guqin Tuning: 
     F Pentatonic with string steps: [0, 1, 1, 1, 1, 1, 1], beginning at scale degree 3:
-    C - D - F - G - A - C - D (Seventh String added to match grid dimensions)
+    C - D - F - G - A - C - D (Seventh String Added to match grid dimensions)
 */
 
 // double check that PC to Keynum and Keynum to PC functions return the correct octave
@@ -28,43 +29,43 @@ EG: Standard Guqin Tuning:
 export class FretBoard {
   name: string
   string_steps: number[]
-  scaledegree: number
+  num_frets: number
+  scale_degree: number
   tonic: PitchClass
   mode: number[]
   frets: number[][]
 
-  constructor(name: string, string_steps: number[], scaledegree: number, tonic: PitchClass, mode: number[]) {
+  constructor(name: string, string_steps: number[], num_frets: number, scale_degree: number, tonic: PitchClass, mode: number[]) {
     this.name = name
     this.string_steps = string_steps
-    this.scaledegree = scaledegree
+    this.num_frets = num_frets
+    this.scale_degree = scale_degree
     this.tonic = tonic
     this.mode = mode  
     this.frets = []    
   }
-    
-  // i values corresponds to zither strings, j  values corresponds to zither frets
-    
+  
   calculateFrets(): number[][] {
     var notearr =  Array()   
 
-    for (var j = 0; j < this.mode.length; j++) {
+    for (var j = 0; j < this.num_frets; j++) {
       var step_sum: number = 0
       var notes: number[] = [] 
        
-    // calculate and assign values for each fret
+    // calculate and assign values for each j fret
 
     for (var i = 0; i < this.string_steps.length; i++) {
       step_sum = step_sum + this.string_steps[i] 
   
-      var current_note = this.tonic.modalTranspositionPC(this.scaledegree+j, this.tonic, this.mode)
+      var current_note = this.tonic.modalTransposition(this.scale_degree+j, this.tonic, this.mode)
 
       var total_steps: number = num_steps_from_scale_degree(
-        this.scaledegree+j, //increment scale degree for the jth fret
+        this.scale_degree+j, //increment scale degree for the jth fret
         step_sum,
         this.tonic, //add scale degree designation here
         this.mode
       )
-         notes[i] =  current_note.pcToKeynum() + total_steps   
+         notes[i] =  current_note + total_steps   
     }
     notearr.push(notes);
   }
@@ -73,4 +74,5 @@ export class FretBoard {
 
   return  notearr  
   }
+
 }
